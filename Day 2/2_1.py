1c75c12a99e55039bbeda4402864fa2fb847862b
GoranAlasov <alasov.jr@gmail.com>
2021-12-02 15:49:48 +0100

    a small fix in day 2 task 1

--- Day 2/2_1.py
+++ Day 2/2_1.py
@@ -3,8 +3,10 @@ from decoder_2_1 import decoder
 file = open("navigation.txt", "r")
 lines = file.read().splitlines()
 
-horizontal = decoder(lines)[0]
-vertical = decoder(lines)[1]
+decoded = decoder(lines)
+
+horizontal = decoder[0]
+vertical = decoder[1]
 
 horizontal_sum = sum(horizontal)
 vertical_sum = sum(vertical)
